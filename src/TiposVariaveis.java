e09482144d2868ef4392fb806624a97e01b97460
THIAGO305 <thiagofarias0df@gmail.com>
2023-07-10 15:17:48 -0300

    Operadores3

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+       short NUMERO = 1;
+        NUMERO = 2;
+        int numeronormal = NUMERO;
+        short numero2 = (short) numeronormal;
+
+        System.out.println(numero2);
+
+    }
+}

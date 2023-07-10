d9b37c3495ef84a28d3ebac114ea98806fd06a1f
THIAGO305 <thiagofarias0df@gmail.com>
2023-07-10 16:16:40 -0300

    Operador Relacional

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,11 +1,26 @@
 public class Operadores {
     public static void main(String[] args) {
-        int a, b;
+        String nomeUm = "thiago";
+        String nomedois = new String("thiago");
 
-        a = 5;
-        b = 5;
-        String resultado = (a == b) ? "verdadeiro" : "false";
+        System.out.print( nomeUm.equals(nomedois));
 
-        System.out.println(resultado);
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.print("Numero 1 é diferente de numero 2");
     }
 }

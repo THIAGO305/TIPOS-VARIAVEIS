9c9f63f2a5e76e19587b762f6dda91532c1cde51
THIAGO305 <thiagofarias0df@gmail.com>
2023-07-10 16:21:34 -0300

    operadores lógicos

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,26 +1,17 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeUm = "thiago";
-        String nomedois = new String("thiago");
+        boolean condicao1 = false;
 
-        System.out.print( nomeUm.equals(nomedois));
+        boolean condicao2 = false;
 
-        int numero1 = 1;
-        int numero2 = 2;
+        if (condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");
+        ;
 
-        if(numero1 > numero2)
-            System.out.print("Numero 1 maior que numero 2");
-
-        if(numero1 < numero2)
-            System.out.print("Numero 1 menor que numero 2");
-
-        if(numero1 >= numero2)
-            System.out.print("Numero 1 maior ou igual que numero 2");
-
-        if(numero1 <= numero2)
-            System.out.print("Numero 1 menor ou igual que numero 2");
-
-        if(numero1 != numero2)
-            System.out.print("Numero 1 é diferente de numero 2");
+//Se condicao1 OU condicao2 for verdadeira, executar código.
+        if (condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
+        System.out.print("fim");
     }
+
 }
